4e06124b5eed29db1958b1eed2ca1a85e2c79daa
Tal Shachar <talshlomo.shachar@post.runi.ac.il>
2024-08-29 00:47:30 +0300

    Correct STOP_CODON abbreviation

--- src/RNA/control_codons.py
+++ src/RNA/control_codons.py
@@ -2,7 +2,7 @@ from .codon_pattern import CodonPattern
 from .nucleotide import Nucleotide
 
 # UAA | UAG | UGA - A codon signaling the termination of construction and release of polypeptide strand
-STOP_CODON = CodonPattern('H', 'Stop Transcription', (Nucleotide.U, Nucleotide.A, Nucleotide.A | Nucleotide.G),
+STOP_CODON = CodonPattern('*', 'Stop Transcription', (Nucleotide.U, Nucleotide.A, Nucleotide.A | Nucleotide.G),
                    (Nucleotide.U, Nucleotide.G, Nucleotide.A))
 
 # AUG - A codon signaling the beginning of construction of polypeptide strand
